68e37aa51688fa4ff9dbbe3910c87dce2477c6fe
Yida8676 <74218375+Yida8676@users.noreply.github.com>
2020-12-06 15:40:46 +0800

    Add files via upload

---
+++ content/blog.js
@@ -0,0 +1,18 @@
+const blog = [
+    {
+        id: 1,
+        title: "这是我的第一篇博客",
+        image: "https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
+    },
+    {
+        id: 2,
+        title: "这是我的第二篇博客",
+        image: "https://images.unsplash.com/photo-1551970634-747846a548cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80"
+    },
+    {
+        id: 3,
+        title: "这是我的第三篇博客",
+        image: "https://images.unsplash.com/photo-1506634064465-7dab4de896ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+    }
+
+]
